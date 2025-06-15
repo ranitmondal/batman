3b5e2dcc35001ebcb4d2393544c988b0df1a6067
ranitmondal111 <ranitmondal111@gmail.com>
2025-06-15 17:45:18 +0000

    first commit

---
+++ src/main/java/com/example/demo/Queus.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Main {
+    public static void main(String[] args) {
+        Queue<String> myQueue = new LinkedList<String>();
+
+        //Queue run as FIFO
+
+        System.out.println("The queue is empty: " + myQueue.isEmpty());
+        //adding items to queue
+        myQueue.offer("Abcd");
+        myQueue.offer("efgh");
+        myQueue.offer("ijkl");
+        myQueue.offer("mnop");
+
+        System.out.println("Adding items to queue: " + myQueue);
+
+        //removing items from queue
+        myQueue.poll();   
+        System.out.println("Removing head from the queue: " +myQueue);
+
+
+    }
+}
---
+++ src/main/java/com/example/demo/Stacks.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) {
+        Stack<String> myStack = new Stack<String>();
+
+        //Stack run as LIFO
+        
+
+
+        //pushig items into stack
+        myStack.push("Abcd");
+        myStack.push("efgh");
+        myStack.push("ijkl");
+        myStack.push("mnop");
+
+        System.out.println(myStack);
+
+
+
+
+
+        //removing items from stack
+        myStack.pop();
+
+
+        System.out.println(myStack);
+
+    }
+}
